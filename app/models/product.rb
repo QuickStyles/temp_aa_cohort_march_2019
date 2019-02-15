74e4424832abe0ccc08bbccbe6fc4f05e688ee2e
Brandon Lam <brandonywlam@gmail.com>
2019-02-15 15:37:15 -0800

    Product model callbacks

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,17 @@ class Product < ApplicationRecord
   validates(:title, presence: true, uniqueness: true, case_sensitive: false)
   validates(:price, numericality: { greater_than: 0 })
   validates(:description, presence: true, length: { minimum: 10 })
+  before_validation :set_default_value_price
+  before_save :capitalize_product_title
+  # validate: (:set_default_value_price)
+
+  private
+
+  def set_default_value_price
+    self.price ||= 1
+  end
+
+  def capitalize_product_title
+    self.title.capitalize!
+  end
 end
