f093db62e883646addc2e7246649b249f8ccadec
Brett Crowe <crowe706@gmail.com>
2019-02-26 07:33:06 -0800

    Users Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
+
   resources :products do
     # creates the following route for us:
     # post('/products/:product_id/reviews', { to: 'reviews#create', as: :product_reviews })
