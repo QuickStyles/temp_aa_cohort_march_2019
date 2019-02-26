51729296bbf0f9e5edb5d759f051c8e4eeafd143
Brett Crowe <crowe706@gmail.com>
2019-02-26 07:11:38 -0800

    Reviews Controller Create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products
+  resources :products do
+    # creates the following route for us:
+    # post('/products/:product_id/reviews', { to: 'reviews#create', as: :product_reviews })
+    # Which, due to the `as` creates a method called `product_reviews_path`
+    # This method requires one variable, a product or product id to "fill in"
+    # the value for `:product_id` in the path
+    # It returns the value: '/products/:product_id/reviews' with the :product_id
+    # "filled in"
+    resources :reviews, shallow: true, only: [:create]
+  end
 
   root 'welcome#index'
   get('/about', { to: 'welcome#about', as: :about })
